609aaba9a8b29f42c601801b71febfcd71574bb8
dknelson9876 <dknelson9876@gmail.com>
2023-11-04 18:34:24 -0600

    Custom send and receive

--- scmb_serial.py
+++ scmb_serial.py
@@ -1,6 +1,5 @@
 import serial
 
-
 def find_available_serial_ports() -> list:
     available_ports = []
     try:
@@ -12,7 +11,6 @@ def find_available_serial_ports() -> list:
         for port in serial.tools.list_ports.comports():
             available_ports.append((port.device, port.description))
     return available_ports
-
 
 def select_serial_port(available_ports):
     if not available_ports:
@@ -26,28 +24,74 @@ def select_serial_port(available_ports):
     while True:
         try:
             selection = int(
-                input(f"Select a serial port (0 - {len(available_ports)-1}): "))
-            if 0 <= selection <= len(available_ports)-1:
+                input(f"Select a serial port (0 - {len(available_ports) - 1}): "))
+            if 0 <= selection <= len(available_ports) - 1:
                 return available_ports[selection][0]
             else:
                 print("Invalid selection. Please choose a valid port number.")
         except ValueError:
             print("Invalid input. Please enter a number.")
 
-
-def send_hello_world(port):
-    if port is None:
-        return
-
+def send_message(ser, message):
     try:
-        with serial.Serial(port, 115200, timeout=1) as ser:
-            ser.write(b"hello world")
-            print("Sent 'hello world' to the serial device.")
+        ser.write(message)
+        formatted_message = ''.join([f'{byte:02X}' for byte in message])
+        print(f"Sent: 0x{formatted_message}")
     except serial.SerialException as e:
         print(f"Error communicating with the serial device: {e}")
 
+def receive_response(ser):
+    try:
+        response = ser.read(12)
+        return response
+    except serial.SerialException as e:
+        print(f"Error communicating with the serial device: {e}")
+        return None
+
+def display_menu(messages, ser):
+    while True:
+        print("Select an option:")
+        for i, (description, hex_value) in enumerate(messages):
+            formatted_hex_value = ''.join([f'{byte:02X}' for byte in hex_value])
+            print(f"{i}: {description} - 0x{formatted_hex_value}")
+        print("q: Quit")
+
+        user_input = input("Enter your choice: ")
+
+        if user_input == 'q':
+            break
+
+        try:
+            choice = int(user_input)
+            if 0 <= choice < len(messages):
+                send_message(ser, messages[choice][1])
+                response = receive_response(ser)
+                if response:
+                    formatted_response = ''.join([f'{byte:02X}' for byte in response])
+                    print(f"Received: 0x{formatted_response}")
+            else:
+                print("Invalid option. Please choose a valid number.")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
 
 if __name__ == "__main__":
     available_ports = find_available_serial_ports()
     selected_port = select_serial_port(available_ports)
-    send_hello_world(selected_port)
+    ser = None
+
+    if selected_port:
+        try:
+            ser = serial.Serial(selected_port, 115200, timeout=1)
+            messages = [
+                ("Set regA=5", b"\xF2\x84\x00\x00\x00\x00\x05"),
+                ("Set regB=0", b"\xF2\x85\x00\x00\x00\x00\x00"),
+                # Add more messages as needed
+            ]
+            display_menu(messages, ser)
+        except serial.SerialException as e:
+            print(f"Error opening the serial port: {e}")
+        finally:
+            if ser:
+                ser.close()
+    else:
+        print("No serial port selected. Exiting.")
